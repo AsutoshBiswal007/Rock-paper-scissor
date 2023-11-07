428d37a680cc573f93c8f85b8bcd03fac1740a7f
Asutosh Biswal <asutoshbiswal007@gmail.com>
2023-11-07 10:55:17 +0530

    js file created

---
+++ game.js
@@ -0,0 +1,17 @@
+let computerChoice;
+function generateComputerChoice(){
+    let randomNumber = Math.random()* 3;
+    if(randomNumber>0 && randomNumber<=1){
+         computerChoice='Stone';
+        //console.log('Computer Choice is Stone')
+    }
+    else if (randomNumber>1 && randomNumber<=2){
+        computerChoice = 'Paper';
+        //console.log('Computer Choice is Paper')
+    }
+    
+    else{
+         computerChoice = 'Scissor';
+        //console.log('Computer Choice is Scissor')
+    }
+}
